ebf06d135a30ba4bd1ceebe3439b4282ecf26b44
zhgkj <zhgkaijun@gmail.com>
2022-08-27 14:18:04 -0400

    port

--- src/index.js
+++ src/index.js
@@ -366,7 +366,7 @@ const start = async () => {
   } });
 
   // The `listen` method launches a web server.
-  server.listen().then(({ url }) => {
+  server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
   }); 
 }
